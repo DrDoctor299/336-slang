de65cd44e3ea594eb6f0ec077c3c543d2e47b90d
DrDoctor299 <joshuwilliams@csumb.edu>
2018-12-10 23:40:19 +0000

    fixing modal id

--- index.php
+++ index.php
@@ -75,11 +75,11 @@ include "checkSupportedLanguages.php";
           <div class="contributeBox" id="displayUserContributions"></div>
         </div>
         <!--Contribute edit modal-->
-        <div id="myModal" class="modal fade" role="dialog">
+        <div id="editModal" class="modal fade" role="dialog">
           <div class="modal-dialog">
         
             <!-- Modal content-->
-            <div class="modal-content" id="editModal">
+            <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Edit Contribution</h4>
