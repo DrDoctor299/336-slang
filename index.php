edc2b46fdbeec945e28dea0b85b5186a550a87a6
DrDoctor299 <joshuwilliams@csumb.edu>
2018-12-11 02:29:47 +0000

    restyled the modal to be larger (and prettier)

--- index.php
+++ index.php
@@ -81,13 +81,13 @@ include "checkSupportedLanguages.php";
         </div>
         <!--Contributons edit modal-->
         <div id="editModal" class="modal fade" role="dialog">
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-lg">
         
             <!-- Modal content-->
             <div class="modal-content">
               <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Edit Contribution</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
               </div>
               <div class="modal-body">
                 <input id="editTextOne" type="text" placeholder="First Language Pair"></input>
